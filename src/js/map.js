dbe55a54e6a4124f058d742d5acd6d89d48d81ca
Jamie Suliivan <jamiesullivan523@gmail.com>
2018-03-02 20:33:17 +0000

    Rearranged SASS file Structure
    Organised stylesheets into folders. Reset font sizes to default size. Prepared code for responsive development.

--- src/js/map.js
+++ src/js/map.js
@@ -2,9 +2,10 @@ function myMap() {
   var location = {lat: 53.350733, lng: -6.283442}
 
   var mapOptions = {
-      center: new google.maps.LatLng(location),
-      zoom: 17,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: new google.maps.LatLng(location),
+    zoom: 17,
+    scrollwheel: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   }
 
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
